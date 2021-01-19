a0328bf8b409862b5c3e1b49a3c09b546a905d9b
Kristoffer-Martinsen <kristoffermartinsen.95@gmail.com>
2021-01-19 17:04:43 +0100

    Started implementing the list items in board

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import {Card, CardGroup} from 'react-bootstrap';
+import React, {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import {Card, CardDeck} from 'react-bootstrap';
+
+const todoItems = [
+
+];
+
+const inProgressItems = [
+
+];
+
+const doneItems = [
+
+];
+
 
 export default function Board() {
+
+    const [todoList, setTodoList] = useState(todoItems);
+    const [todoName, setTodoname] = useState('');
+    const [inProgressList, setInProgressList] = useState(inProgressItems);
+    const [inProgressName, setInProgressName] = useState('');
+    const [doneList, setDoneItems] = useState(doneItems);
+    const [doneName, setDoneName] = useState('');
+
+    function handleAddTodoItem() {
+        const newTodoList = todoList.concat({todoName, id: uuidv4})
+        setTodoList(newTodoList);
+    }
+
     return (
         <div>
-            <CardGroup>
-                <Card>
+            <CardDeck>
+                <Card >
                     <Card.Body>
                         <Card.Title>To Do</Card.Title>
                         <Card.Text>test</Card.Text>
@@ -23,8 +50,7 @@ export default function Board() {
                         <Card.Text>test</Card.Text>
                     </Card.Body>
                 </Card>
-                
-            </CardGroup>
+            </CardDeck>
         </div>
     )
 }
